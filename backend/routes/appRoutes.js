09cc4257c9167f3e2401f0dfaf47c62962072ee7
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-12-12 00:34:16 +0530

    appRoutes Added

--- backend/routes/appRoutes.js
+++ backend/routes/appRoutes.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const { getApplications, handleAudit } = require('../controllers/appController');
 
+const router = express.Router();
+
+// Routes
+router.get('/applications', getApplications);
+router.post('/audit', handleAudit);
+
+module.exports = router;
