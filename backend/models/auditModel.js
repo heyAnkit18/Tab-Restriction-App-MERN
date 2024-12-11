60a25d2577407602068ebea6396af8e91803dfdc
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-12-12 00:33:08 +0530

    auditModel.js Added

---
+++ backend/models/auditModel.js
@@ -0,0 +1,10 @@
+// models/auditModel.js
+const mongoose = require('mongoose');
+
+const auditSchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  action: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('AuditLog', auditSchema);
